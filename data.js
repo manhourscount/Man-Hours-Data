4e6e0db3d0f2e917039b940dfbd8c2b3cf050385
manhourscount <ron.3270@gmail.com>
2026-05-29 20:45:08 +0800

    Add files via upload

---
+++ data.js
@@ -0,0 +1,18 @@
+// data.js - Dedicated metrics storage configuration
+const dashboardData = {
+  'LIVE': { male: 223, female: 228, absentDays: 0, shiftLength: 8 },
+  'MTD':  { male: 210, female: 215, absentDays: 5, shiftLength: 8 },
+  'YTD':  { male: 195, female: 200, absentDays: 45, shiftLength: 8 },
+  'M-0':  { male: 180, female: 190, absentDays: 12, shiftLength: 8 }, // Jan
+  'M-1':  { male: 185, female: 192, absentDays: 8,  shiftLength: 8 }, // Feb
+  'M-2':  { male: 190, female: 195, absentDays: 10, shiftLength: 8 }, // Mar
+  'M-3':  { male: 200, female: 210, absentDays: 6,  shiftLength: 8 }, // Apr
+  'M-4':  { male: 223, female: 228, absentDays: 4,  shiftLength: 8 }, // May
+  'M-5':  { male: 0,   female: 0,   absentDays: 0,  shiftLength: 8 }, // Jun
+  'M-6':  { male: 0,   female: 0,   absentDays: 0,  shiftLength: 8 }, // Jul
+  'M-7':  { male: 0,   female: 0,   absentDays: 0,  shiftLength: 8 }, // Aug
+  'M-8':  { male: 0,   female: 0,   absentDays: 0,  shiftLength: 8 }, // Sep
+  'M-9':  { male: 0,   female: 0,   absentDays: 0,  shiftLength: 8 }, // Oct
+  'M-10': { male: 0,   female: 0,   absentDays: 0,  shiftLength: 8 }, // Nov
+  'M-11': { male: 0,   female: 0,   absentDays: 0,  shiftLength: 8 }  // Dec
+};
